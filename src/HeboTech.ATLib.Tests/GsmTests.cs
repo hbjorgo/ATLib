5ff34819fadd2a2572b72e15769d088c89cdf33d
Henning Bjørgo <henning.bjorgo@bouvet.no>
2018-06-22 14:22:49 +0200

    Added unit tests.

--- src/HeboTech.ATLib.Tests/GsmTests.cs
+++ src/HeboTech.ATLib.Tests/GsmTests.cs
@@ -24,5 +24,74 @@ namespace HeboTech.ATLib.Tests
                 Assert.True(result);
             }
         }
+
+        [Fact]
+        public void UnlockSimTest()
+        {
+            Pin pin = new Pin(1, 2, 3, 4);
+
+            Encoding enc = Encoding.ASCII;
+            using (TestStream stream = new TestStream(enc))
+            using (GsmStream gsmStream = new GsmStream(stream, enc))
+            {
+                stream.DataWritten += (s, e) =>
+                {
+                    if (e.Data == $"AT+CPIN={pin.ToString()}")
+                        stream.SetReply("\r\nOK\r\n");
+                };
+
+                Gsm g = new Gsm(gsmStream);
+                bool result = g.UnlockSim(pin).Result;
+
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public void SetModeTest()
+        {
+            Mode mode = Mode.Text;
+
+            Encoding enc = Encoding.ASCII;
+            using (TestStream stream = new TestStream(enc))
+            using (GsmStream gsmStream = new GsmStream(stream, enc))
+            {
+                stream.DataWritten += (s, e) =>
+                {
+                    if (e.Data == $"AT+CMGF={(int)mode}\r\n")
+                        stream.SetReply("\r\nOK\r\n");
+                };
+
+                Gsm g = new Gsm(gsmStream);
+                bool result = g.SetMode(mode).Result;
+
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public void SendSmsTest()
+        {
+            string phoneNumber = "12345678";
+            string message = "Msg";
+
+            Encoding enc = Encoding.ASCII;
+            using (TestStream stream = new TestStream(enc))
+            using (GsmStream gsmStream = new GsmStream(stream, enc))
+            {
+                stream.DataWritten += (s, e) =>
+                {
+                    if (e.Data == $"AT+CMGS=\"{phoneNumber}\"\r")
+                        stream.SetReply("> ");
+                    if (e.Data == $"{message}\x1A\r\n")
+                        stream.SetReply("\r\nOK\r\n");
+                };
+
+                Gsm g = new Gsm(gsmStream);
+                bool result = g.SendSmsAsync(phoneNumber, message).Result;
+
+                Assert.True(result);
+            }
+        }
     }
 }
