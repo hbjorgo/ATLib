44c45488e1d10f27caa58937d178db81c5c9c4a3
Henning Bjørgo <henning.bjorgo@bouvet.no>
2018-09-07 09:07:07 +0200

    XML comments.

--- src/HeboTech.ATLib/IGsmStream.cs
+++ src/HeboTech.ATLib/IGsmStream.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <param name="send">The command to send</param>
         /// <param name="expectedReply">The expected reply to wait for</param>
-        /// <param name="timeout">Time timeout in seconds</param>
+        /// <param name="timeout">Time timeout in milliseconds</param>
         /// <returns></returns>
         bool SendCheckReply(string send, string expectedReply, int timeout);
 
@@ -16,7 +16,7 @@
         /// </summary>
         /// <param name="send">The command to send</param>
         /// <param name="terminationReply">The expected termination reply to wait for</param>
-        /// <param name="timeout">The timeout in seconds</param>
+        /// <param name="timeout">The timeout in milliseconds</param>
         /// <returns></returns>
         string SendGetReply(string send, string terminationReply, int timeout);
     }
