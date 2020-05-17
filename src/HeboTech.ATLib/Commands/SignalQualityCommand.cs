9c134d419b4c54f42699d503de2f3d9a45f1cb1f
Henning Bjørgo <henningbjorgo@gmail.com>
2020-05-17 21:26:14 +0200

    ResponseFormat as parameter.

--- src/HeboTech.ATLib/Commands/SignalQualityCommand.cs
+++ src/HeboTech.ATLib/Commands/SignalQualityCommand.cs
@@ -9,16 +9,19 @@ namespace HeboTech.ATLib.Commands
 {
     public static class SignalQualityCommand
     {
-        public static async ValueTask<ATResult<SignalQualityResult>> GetSignalQualityAsync(this ICommunicator<string> comm, CancellationToken cancellationToken = default)
+        public static async ValueTask<ATResult<SignalQualityResult>> GetSignalQualityAsync(
+            this ICommunicator<string> comm,
+            ResponseFormat responseFormat,
+            CancellationToken cancellationToken = default)
         {
             await comm.Write("AT+CSQ\r", cancellationToken);
             var message = await comm.ReadSingleMessageAsync(Constants.BYTE_LF, cancellationToken);
-            if (SignalQualityParser.TryParse(message, ResponseFormat.Numeric, out ATResult<SignalQualityResult> signalQualityResult))
+            if (SignalQualityParser.TryParse(message, responseFormat, out ATResult<SignalQualityResult> signalQualityResult))
             {
                 message = await comm.ReadSingleMessageAsync(Constants.BYTE_LF, cancellationToken);
-                if (OkParser.TryParse(message, ResponseFormat.Numeric, out ATResult<OkResult> _))
+                if (OkParser.TryParse(message, responseFormat, out ATResult<OkResult> _))
                     return signalQualityResult;
-                else if (ErrorParser.TryParse(message, ResponseFormat.Numeric, out ATResult<ErrorResult> errorResult))
+                else if (ErrorParser.TryParse(message, responseFormat, out ATResult<ErrorResult> errorResult))
                     return ATResult.Error<SignalQualityResult>(errorResult.ToString());
             }
             return ATResult.Error<SignalQualityResult>(Constants.PARSING_FAILED);
