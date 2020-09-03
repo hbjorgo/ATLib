12815f40e404bcb26a40642b5f0afedcf0c1540c
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2020-09-03 19:55:20 +0200

    Improvements (#4)
    * Using ring buffer.
    
    * Using tasks.
    
    * Fixed tests.

--- src/HeboTech.ATLib.Tests/Parsers/AtLineReaderTests.cs
+++ src/HeboTech.ATLib.Tests/Parsers/AtLineReaderTests.cs
@@ -10,20 +10,20 @@ namespace HeboTech.ATLib.Tests.Parsers
     public class AtLineReaderTests
     {
         [TestMethod]
-        public void OkResultTest()
+        public async Task OkResultTestAsync()
         {
             var commMock = new CommunicatorMock();
             commMock.AppendReturnValue("\r\nOK\r\n");
            
             AtLineReader dut = new AtLineReader(commMock);
 
-            string result = dut.ReadLine();
+            string result = await dut.ReadLineAsync();
 
             Assert.AreEqual("OK", result);
         }
 
         [TestMethod]
-        public void SimReadyTest()
+        public async Task SimReadyTestAsync()
         {
             var commMock = new CommunicatorMock();
             commMock.AppendReturnValue("\r\n\r\n\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n+CP READY\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n+SPIC: 3,10,1,10\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n+CSQ: 16,99\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n+CBC: 0,100,4.232V\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\nManufacturer: SIMCOM INCORPORATED\r\nModel: SIMCOM_SIM5320E\r\nRevision: SIM5320E_V1.5\r\nIMEI: 012345678901234\r\n+GCAP: +CGSM,+DS,+ES\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n");
@@ -32,54 +32,54 @@ namespace HeboTech.ATLib.Tests.Parsers
 
             string result;
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CP READY", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+SPIC: 3,10,1,10", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CSQ: 16,99", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CBC: 0,100,4.232V", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Manufacturer: SIMCOM INCORPORATED", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Model: SIMCOM_SIM5320E", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Revision: SIM5320E_V1.5", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("IMEI: 012345678901234", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+GCAP: +CGSM,+DS,+ES", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
         }
 
         [TestMethod]
-        public void SimPinTest()
+        public async Task SimPinTestAsync()
         {
             var commMock = new CommunicatorMock();
             commMock.AppendReturnValue("\r\n\r\nSTART\r\n\r\n+ST 25\r\n\r\n+ST 25\r\n\r\n+CP SIM PIN\r\n\r\n\r\nATE0\r\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n+CP SIM PIN\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n+SPIC: 3,10,1,10\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n+CP READY\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nSMS DONE\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nPB DONE\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n+CP READY\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n+CSQ: 16,99\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n+CBC: 0,100,4.232V\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\nManufacturer: SIMCOM INCORPORATED\r\nModel: SIMCOM_SIM5320E\r\nRevision: SIM5320E_V1.5\r\nIMEI: 012345678901234\r\n+GCAP: +CGSM,+DS,+ES\r\n\r\nOK\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n");
@@ -88,82 +88,82 @@ namespace HeboTech.ATLib.Tests.Parsers
 
             string result;
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("START", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+ST 25", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+ST 25", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CP SIM PIN", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("ATE0", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CP SIM PIN", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+SPIC: 3,10,1,10", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CP READY", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("SMS DONE", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("PB DONE", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CP READY", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CSQ: 16,99", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+CBC: 0,100,4.232V", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Manufacturer: SIMCOM INCORPORATED", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Model: SIMCOM_SIM5320E", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("Revision: SIM5320E_V1.5", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("IMEI: 012345678901234", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("+GCAP: +CGSM,+DS,+ES", result);
 
-            result = dut.ReadLine();
+            result = await dut.ReadLineAsync();
             Assert.AreEqual("OK", result);
         }
 
