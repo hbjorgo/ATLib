501365f5c022474828b951791e6970feff271b27
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2021-03-30 21:58:31 +0200

    Updated comments.

--- src/HeboTech.ATLib/Modems/Adafruit/AdafruitFona3G.cs
+++ src/HeboTech.ATLib/Modems/Adafruit/AdafruitFona3G.cs
@@ -5,6 +5,12 @@ namespace HeboTech.ATLib.Modems.Adafruit
 {
     public class AdafruitFona3G : SIMCOM_SIM5320
     {
+        /// <summary>
+        /// Based on SIMCOM SIM5320 chipset
+        /// 
+        /// Serial port settings:
+        /// 9600 8N1 Handshake.None
+        /// </summary>
         public AdafruitFona3G(AtChannel channel)
             : base(channel)
         {
--- src/HeboTech.ATLib/Modems/D_LINK/DWM_222.cs
+++ src/HeboTech.ATLib/Modems/D_LINK/DWM_222.cs
@@ -3,14 +3,14 @@ using HeboTech.ATLib.Parsers;
 
 namespace HeboTech.ATLib.Modems.D_LINK
 {
-    /// <summary>
-    /// Based on Qualcomm MDM9225 chipset
-    /// 
-    /// Serial port settings:
-    /// 9600 8N1, Handshake.RequestToSend
-    /// </summary>
     public class DWM_222 : MDM9225
     {
+        /// <summary>
+        /// Based on Qualcomm MDM9225 chipset
+        /// 
+        /// Serial port settings:
+        /// 9600 8N1 Handshake.RequestToSend
+        /// </summary>
         public DWM_222(AtChannel channel)
             : base(channel)
         {
