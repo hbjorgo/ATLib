00b6cac5c7ae13e60c7400ffe814775e95ea1b70
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2021-04-07 19:13:21 +0200

    Fixes

--- src/HeboTech.ATLib/Parsers/AtChannel.cs
+++ src/HeboTech.ATLib/Parsers/AtChannel.cs
@@ -400,16 +400,10 @@ namespace HeboTech.ATLib.Parsers
                         }
 
                         // There's no more data to be processed.
-                        //if (result.IsCompleted)
-                        //{
-                        //    if (buffer.Length > 0)
-                        //    {
-                        //        // The message is incomplete and there's no more data to process.
-                        //        throw new InvalidDataException("Incomplete message.");
-                        //    }
-
-                        //    break;
-                        //}
+                        if (result.IsCompleted)
+                        {
+                            break;
+                        }
                     }
                     finally
                     {
