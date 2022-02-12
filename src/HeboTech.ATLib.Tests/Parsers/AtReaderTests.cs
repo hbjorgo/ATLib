035b48ffd0b5dea52cd20048c609de484917ecd6
Henning Bjørgo <henningbjorgo@gmail.com>
2022-02-12 12:23:09 +0100

    Unit tests

--- src/HeboTech.ATLib.Tests/Parsers/AtReaderTests.cs
+++ src/HeboTech.ATLib.Tests/Parsers/AtReaderTests.cs
@@ -57,5 +57,77 @@ namespace HeboTech.ATLib.Tests.Parsers
             Assert.Equal("> ", smsPrompt);
             Assert.Equal("Line3", line3);
         }
+
+        [Fact]
+        public async Task Empty_lines_are_readAsync()
+        {
+            MemoryStream stream = new MemoryStream(512);
+            AtReader dut = new(stream);
+
+            string input = "\r\n\r\n\r\n";
+            byte[] buffer = Encoding.UTF8.GetBytes(input);
+            stream.Write(buffer);
+            stream.Position = 0;
+
+            dut.Open();
+
+            string line1 = await dut.ReadAsync();
+            string line2 = await dut.ReadAsync();
+            string line3 = await dut.ReadAsync();
+
+            dut.Close();
+
+            Assert.Equal("", line1);
+            Assert.Equal("", line2);
+            Assert.Equal("", line3);
+        }
+
+        [Fact]
+        public async Task Cme_Error_is_readAsync()
+        {
+            MemoryStream stream = new MemoryStream(512);
+            AtReader dut = new(stream);
+
+            string input = "+CME ERROR: ErrorMessage\r\n";
+            byte[] buffer = Encoding.UTF8.GetBytes(input);
+            stream.Write(buffer);
+            stream.Position = 0;
+
+            dut.Open();
+
+            string line1 = await dut.ReadAsync();
+
+            dut.Close();
+
+            Assert.Equal("+CME ERROR: ErrorMessage", line1);
+        }
+
+        [Fact]
+        public async Task Ring_is_readAsync()
+        {
+            MemoryStream stream = new MemoryStream(512);
+            AtReader dut = new(stream);
+
+            string input = "RING\r\n\r\nRING\r\n\r\nMISSED_CALL: 01:23PM 12345678\r\n";
+            byte[] buffer = Encoding.UTF8.GetBytes(input);
+            stream.Write(buffer);
+            stream.Position = 0;
+
+            dut.Open();
+
+            string line1 = await dut.ReadAsync();
+            string line2 = await dut.ReadAsync();
+            string line3 = await dut.ReadAsync();
+            string line4 = await dut.ReadAsync();
+            string line5 = await dut.ReadAsync();
+
+            dut.Close();
+
+            Assert.Equal("RING", line1);
+            Assert.Equal("", line2);
+            Assert.Equal("RING", line3);
+            Assert.Equal("", line4);
+            Assert.Equal("MISSED_CALL: 01:23PM 12345678", line5);
+        }
     }
 }
