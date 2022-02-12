6631be11735043de693743b3857a9c5bcaf7f20e
Henning Bjørgo <henningbjorgo@gmail.com>
2022-02-12 23:23:05 +0100

    Pdu test

--- src/HeboTech.ATLib.Tests/PDU/PduTests.cs
+++ src/HeboTech.ATLib.Tests/PDU/PduTests.cs
@@ -6,8 +6,8 @@ namespace HeboTech.ATLib.Tests.PDU
     public class PduTests
     {
         [Theory]
-        [InlineData("07917238010010F5040BC87238880900F10000993092516195800AE8329BFD4697D9EC37", "+27831000015", "27838890001", "99-03-29-15-16-59-+08")]
-        public void OkTest(string data, string serviceCenterNumber, string senderNumber, string timestamp)
+        [InlineData("07917238010010F5040BC87238880900F10000993092516195800AE8329BFD4697D9EC37", "+27831000015", "+27838890001", "99-03-29-15-16-59-+08", "hellohello")]
+        public void OkTest(string data, string serviceCenterNumber, string senderNumber, string timestamp, string message)
         {
             PduMessage pduMessage = Pdu.Decode(data);
 
@@ -15,6 +15,7 @@ namespace HeboTech.ATLib.Tests.PDU
             Assert.Equal(serviceCenterNumber, pduMessage.ServiceCenterNumber);
             Assert.Equal(senderNumber, pduMessage.SenderNumber);
             Assert.Equal(timestamp, pduMessage.Timestamp.ToString("yy-MM-dd-HH-mm-ss-zz"));
+            Assert.Equal(message, pduMessage.Message);
         }
     }
 }
