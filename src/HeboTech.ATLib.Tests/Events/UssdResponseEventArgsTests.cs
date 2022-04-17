638dd852ae17b8f78113de6ce1221abd2fa0cd2f
Henning Bjørgo <henningbjorgo@gmail.com>
2022-04-17 21:51:41 +0200

    Add support for multiline USSD response

---
+++ src/HeboTech.ATLib.Tests/Events/UssdResponseEventArgsTests.cs
@@ -0,0 +1,32 @@
+﻿using HeboTech.ATLib.Events;
+using Xunit;
+
+namespace HeboTech.ATLib.Tests.Events
+{
+    public class UssdResponseEventArgsTests
+    {
+        [Fact]
+        public void CreateFromResponse_accepts_singleline_response()
+        {
+            string response = "+CUSD: 1,\"Single line response\",15";
+
+            UssdResponseEventArgs dut = UssdResponseEventArgs.CreateFromResponse(response);
+
+            Assert.Equal(1, dut.Status);
+            Assert.Equal("Single line response", dut.Response);
+            Assert.Equal(15, dut.CodingScheme);
+        }
+
+        [Fact]
+        public void CreateFromResponse_accepts_multiline_response()
+        {
+            string response = "+CUSD: 1,\"Line1\r\nLine 2\r\nLine 3\r\nLine 4\r\nLine 5\r\nLine 5\r\nLine 6\",15";
+            
+            UssdResponseEventArgs dut = UssdResponseEventArgs.CreateFromResponse(response);
+
+            Assert.Equal(1, dut.Status);
+            Assert.Equal("Line1\r\nLine 2\r\nLine 3\r\nLine 4\r\nLine 5\r\nLine 5\r\nLine 6", dut.Response);
+            Assert.Equal(15, dut.CodingScheme);
+        }
+    }
+}
