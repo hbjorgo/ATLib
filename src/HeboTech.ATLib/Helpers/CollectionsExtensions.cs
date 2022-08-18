a44b8b74c7a66e8abc0dc1d42144114a5f46be92
Maxime SALIDO <3192885+n-tiz@users.noreply.github.com>
2022-08-18 14:00:37 +0200

    Extensions method replacing usage of range operator

---
+++ src/HeboTech.ATLib/Helpers/CollectionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HeboTech.ATLib.Helpers
+{
+
+    public static class CollectionsExtensions
+    {
+        public static T[] Sub<T>(this T[] self, int start, int end)
+        {
+            var length = end - start;
+            T[] result = new T[length];
+            Array.Copy(self, start, result, 0, length);
+            return result;
+        }
+        public static ReadOnlySpan<T> Sub<T>(this ReadOnlySpan<T> self, int start, int end)
+        {
+            return self.Slice(start, end - start);
+        }
+
+        public static ReadOnlySpan<T> Sub<T>(this ReadOnlySpan<T> self, int start) => self.Sub(start, self.Length);
+    }
+    
+}
