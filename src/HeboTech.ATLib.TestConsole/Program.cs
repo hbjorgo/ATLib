cfbcc76f227ec2e38f9e44eeeaaa5626282ae859
Henning Bjørgo <henning.bjorgo@fakegitemail.com>
2022-09-18 15:06:03 +0200

    Revert

--- src/HeboTech.ATLib.TestConsole/Program.cs
+++ src/HeboTech.ATLib.TestConsole/Program.cs
@@ -28,22 +28,22 @@ namespace HeboTech.ATLib.TestConsole
 
 
             /* ######## UNCOMMENT THIS SECTION TO USE SERIAL PORT ######## */
-            //using SerialPort serialPort = new("COM1", 9600, Parity.None, 8, StopBits.One)
-            //{
-            //    Handshake = Handshake.RequestToSend
-            //};
-            //serialPort.Open();
-            //Console.WriteLine("Serialport opened");
-            //Stream stream;
-            //stream = serialPort.BaseStream;
+            using SerialPort serialPort = new("COM1", 9600, Parity.None, 8, StopBits.One)
+            {
+                Handshake = Handshake.RequestToSend
+            };
+            serialPort.Open();
+            Console.WriteLine("Serialport opened");
+            Stream stream;
+            stream = serialPort.BaseStream;
 
 
             /* ######## UNCOMMENT THIS SECTION TO USE NETWORK SOCKET ######## */
-            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect("192.168.10.129", 7000);
-            Console.WriteLine("Network socket opened");
-            Stream stream;
-            stream = new NetworkStream(socket);
+            //using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //socket.Connect("192.168.10.144", 7000);
+            //Console.WriteLine("Network socket opened");
+            //Stream stream;
+            //stream = new NetworkStream(socket);
 
 
             // ### Choose what to run
