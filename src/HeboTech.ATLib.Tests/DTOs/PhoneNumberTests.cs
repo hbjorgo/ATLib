b136b1dc3caf03d067735837da7d5b5a8d77c0b4
Henning Bjørgo <henning.bjorgo@fakegitemail.com>
2023-06-04 11:13:19 +0200

    Add tests

---
+++ src/HeboTech.ATLib.Tests/DTOs/PhoneNumberTests.cs
@@ -0,0 +1,24 @@
+﻿using HeboTech.ATLib.DTOs;
+using Xunit;
+
+namespace HeboTech.ATLib.Tests.DTOs
+{
+    public class PhoneNumberTests
+    {
+        [Fact]
+        public void Number_property_is_set()
+        {
+            PhoneNumber dut = new("+123456789");
+
+            Assert.Equal("+123456789", dut.Number);
+        }
+
+        [Fact]
+        public void ToString_returns_number()
+        {
+            PhoneNumber dut = new("+123456789");
+
+            Assert.Equal("+123456789", dut.ToString());
+        }
+    }
+}
--- src/HeboTech.ATLib/DTOs/PhoneNumber.cs
+++ src/HeboTech.ATLib/DTOs/PhoneNumber.cs
@@ -14,31 +14,4 @@
             return Number;
         }
     }
-
-    public static class PhoneNumberExtensions
-    {
-        public static TypeOfNumber GetTypeOfNumber(this PhoneNumber phoneNumber)
-        {
-#if NETSTANDARD2_0
-            if (phoneNumber.Number.StartsWith("+"))
-#else
-            if (phoneNumber.Number.StartsWith('+'))
-#endif
-                return TypeOfNumber.International;
-            else
-                return TypeOfNumber.National;
-        }
-
-        public static NumberPlanIdentification GetNumberPlanIdentification(this PhoneNumber phoneNumber)
-        {
-#if NETSTANDARD2_0
-            if (phoneNumber.Number.StartsWith("+"))
-#else
-            if (phoneNumber.Number.StartsWith('+'))
-#endif
-                return NumberPlanIdentification.ISDN;
-            else
-                return NumberPlanIdentification.Unknown;
-        }
-    }
 }
