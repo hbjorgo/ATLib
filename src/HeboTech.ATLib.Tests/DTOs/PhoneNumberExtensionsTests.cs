fc1503201439975c23f29c0d3efdb11733d8e83c
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2023-06-04 11:15:06 +0200

    Correctly handle international phone numbers. (#28)

---
+++ src/HeboTech.ATLib.Tests/DTOs/PhoneNumberExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using HeboTech.ATLib.DTOs;
+using Xunit;
+
+namespace HeboTech.ATLib.Tests.DTOs
+{
+    public class PhoneNumberExtensionsTests
+    {
+        [Fact]
+        public void GetTypeOfNumber_returns_international()
+        {
+            PhoneNumber dut = new("+123456789");
+
+            Assert.Equal(TypeOfNumber.International, dut.GetTypeOfNumber());
+        }
+
+        [Fact]
+        public void GetTypeOfNumber_returns_national()
+        {
+            PhoneNumber dut = new("123456789");
+
+            Assert.Equal(TypeOfNumber.National, dut.GetTypeOfNumber());
+        }
+
+        [Fact]
+        public void GetNumberPlanIdentification_returns_ISDN()
+        {
+            PhoneNumber dut = new("+123456789");
+
+            Assert.Equal(NumberPlanIdentification.ISDN, dut.GetNumberPlanIdentification());
+        }
+
+        [Fact]
+        public void GetNumberPlanIdentification_returns_Unknown()
+        {
+            PhoneNumber dut = new("123456789");
+
+            Assert.Equal(NumberPlanIdentification.Unknown, dut.GetNumberPlanIdentification());
+        }
+    }
+}
--- src/HeboTech.ATLib/DTOs/PhoneNumber.cs
+++ src/HeboTech.ATLib/DTOs/PhoneNumber.cs
@@ -4,33 +4,10 @@
     {
         public PhoneNumber(string number)
         {
-#if NETSTANDARD2_0
-            if (number.StartsWith("+"))
-#else
-            if (number.StartsWith('+'))
-#endif
-            {
-                Ton = TypeOfNumber.International;
-                Npi = NumberPlanIdentification.ISDN;
-            }
-            else
-            {
-                Ton = TypeOfNumber.National;
-                Npi = NumberPlanIdentification.Unknown;
-            }
-            Number = number.TrimStart('+');
-        }
-
-        public PhoneNumber(string number, TypeOfNumber ton, NumberPlanIdentification npi)
-        {
-            Ton = ton;
             Number = number;
-            Npi = npi;
         }
 
-        public TypeOfNumber Ton { get; }
-        public NumberPlanIdentification Npi { get; }
-        public string Number { get; set; }
+        public string Number { get; }
 
         public override string ToString()
         {
