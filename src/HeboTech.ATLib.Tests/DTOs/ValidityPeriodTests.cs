2c19eda54a3fff5848925cb481f19eb912181fca
Henning Bjørgo <henningbjorgo@gmail.com>
2023-10-25 22:02:59 +0200

    ValidityPeriod, almost timezone

---
+++ src/HeboTech.ATLib.Tests/DTOs/ValidityPeriodTests.cs
@@ -0,0 +1,41 @@
+﻿using HeboTech.ATLib.DTOs;
+using System;
+using Xunit;
+
+namespace HeboTech.ATLib.Tests.DTOs
+{
+    public class ValidityPeriodTests
+    {
+        [Fact]
+        public void NotPresent_is_correct()
+        {
+            ValidityPeriod dut = ValidityPeriod.NotPresent();
+
+            Assert.Equal(ValidityPeriodFormat.NotPresent, dut.Format);
+            Assert.Equal(Array.Empty<byte>(), dut.Value);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(127)]
+        [InlineData(255)]
+        public void Relative_is_correct(byte value)
+        {
+            ValidityPeriod dut = ValidityPeriod.Relative(value);
+
+            Assert.Equal(ValidityPeriodFormat.Relative, dut.Format);
+            Assert.Equal(new byte[] { value }, dut.Value);
+        }
+
+        [Theory]
+        [InlineData("2013-03-25 23:01:56 -07:00", new byte[] { 31, 30, 52, 32, 10, 65, 138 })]
+        public void Absolute_is_correct(string dateTimeString, byte[] value)
+        {
+            DateTimeOffset dateTime = DateTimeOffset.Parse(dateTimeString);
+            ValidityPeriod dut = ValidityPeriod.Absolute(dateTime);
+
+            Assert.Equal(ValidityPeriodFormat.Absolute, dut.Format);
+            Assert.Equal(value, dut.Value);
+        }
+    }
+}
--- src/HeboTech.ATLib/DTOs/ValidityPeriod.cs
+++ src/HeboTech.ATLib/DTOs/ValidityPeriod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HeboTech.ATLib.Extensions;
+using System;
 
 namespace HeboTech.ATLib.DTOs
 {
@@ -22,6 +23,43 @@ namespace HeboTech.ATLib.DTOs
         public byte[] Value { get; }
 
         public static ValidityPeriod NotPresent() => new ValidityPeriod(ValidityPeriodFormat.NotPresent, Array.Empty<byte>());
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">
+        /// Value 0-143: (Value + 1) x 5 minutes. Possible values: 5, 10, 15 minutes ... 11:55, 12:00 hours
+        /// Value 144-167: (12 + (Value - 143) / 2) hours. Possible values: 12:30, 13:00, ... 23:30, 24:00 hours
+        /// Value 168-196: (Value - 166) days. Possible values: 2, 3, 4, ... 30 days
+        /// Value 197-255: (Value - 192) weeks. Possible values: 5, 6, 7, ... 63 weeks
+        /// </param>
+        /// <returns></returns>
         public static ValidityPeriod Relative(byte value) => new ValidityPeriod(ValidityPeriodFormat.Relative, new byte[] { value });
+
+        public static ValidityPeriod Absolute(DateTimeOffset value)
+        {
+            byte year = SwapDecimalDigits((byte)(value.Year % 100));
+            byte month = SwapDecimalDigits((byte)value.Month);
+            byte day = SwapDecimalDigits((byte)value.Day);
+            byte hour = SwapDecimalDigits((byte)value.Hour);
+            byte minute = SwapDecimalDigits((byte)value.Minute);
+            byte second = SwapDecimalDigits((byte)value.Second);
+
+            byte timeZoneQuarters = SwapNibbles((byte)(Math.Abs(value.Offset.TotalMinutes) / 15));
+            if (value.Offset.TotalMinutes < 0)
+                timeZoneQuarters |= 0b0000_1000;
+
+            return new ValidityPeriod(ValidityPeriodFormat.Absolute, new byte[] { year, month, day, hour, minute, second, timeZoneQuarters });
+        }
+
+        private static byte SwapDecimalDigits(byte value)
+        {
+            return (byte)(((value % 10) * 10) + (value / 10));
+        }
+
+        private static byte SwapNibbles(byte x)
+        {
+            return (byte)((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+        }
     }
 }
