352336e357d1befe3b6678ebfe168af509883833
Henning Bjørgo <henningbjorgo@gmail.com>
2023-11-03 14:42:17 +0100

    Add MC55i modem.

---
+++ src/HeboTech.ATLib/Modems/Cinterion/MC55i.cs
@@ -0,0 +1,33 @@
+﻿using HeboTech.ATLib.CodingSchemes;
+using HeboTech.ATLib.DTOs;
+using HeboTech.ATLib.Modems.Generic;
+using HeboTech.ATLib.Parsers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HeboTech.ATLib.Modems.Cinterion
+{
+    public class MC55i : ModemBase, IModem
+    {
+        /// <summary>
+        /// Cinterion MC55i chipset
+        /// 
+        /// Serial port settings:
+        /// 9600 8N1 Handshake.None
+        /// </summary>
+        public MC55i(AtChannel channel)
+            : base(channel)
+        {
+        }
+
+        public override Task<IEnumerable<ModemResponse<SmsReference>>> SendSmsInPduFormatAsync(PhoneNumber phoneNumber, string message)
+        {
+            return base.SendSmsInPduFormatAsync(phoneNumber, message, true);
+        }
+
+        public override Task<IEnumerable<ModemResponse<SmsReference>>> SendSmsInPduFormatAsync(PhoneNumber phoneNumber, string message, CodingScheme codingScheme)
+        {
+            return base.SendSmsInPduFormatAsync(phoneNumber, message, codingScheme, true);
+        }
+    }
+}
