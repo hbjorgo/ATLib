ce0a49724d0e2929f69da0df28d20ff462e334e9
Henning Bjørgo <henningbjorgo@gmail.com>
2024-12-15 16:22:20 +0100

    Revert test function

--- src/HeboTech.ATLib.TestConsole/FunctionalityTest.cs
+++ src/HeboTech.ATLib.TestConsole/FunctionalityTest.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HeboTech.ATLib.Modems.Cinterion;
-using HeboTech.ATLib.Modems.Adafruit;
 
 namespace HeboTech.ATLib.TestConsole
 {
